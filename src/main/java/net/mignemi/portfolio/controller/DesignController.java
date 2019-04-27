ff9f8c95d1805393d7b3b7784868dcad56722b87
Unknown <kirby94@hotmail.it>
2019-04-27 11:54:06 +0200

    Request mapping to "design"

--- src/main/java/net/mignemi/portfolio/controller/DesignController.java
+++ src/main/java/net/mignemi/portfolio/controller/DesignController.java
@@ -1,12 +1,14 @@
 package net.mignemi.portfolio.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(value = "/design")
 public class DesignController {
 
-    @RequestMapping("/test")
+    @PostMapping
     public String greeting() {
         return "test";
     }
