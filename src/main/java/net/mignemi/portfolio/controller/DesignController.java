e07cb1566ffeea270ca3980194fb4e906fc17065
Unknown <kirby94@hotmail.it>
2019-04-27 14:13:38 +0200

    saveDesign accepts a title and a multipart file

--- src/main/java/net/mignemi/portfolio/controller/DesignController.java
+++ src/main/java/net/mignemi/portfolio/controller/DesignController.java
@@ -1,18 +1,21 @@
 package net.mignemi.portfolio.controller;
 
-import net.mignemi.portfolio.dto.DesignDto;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping(value = "/design")
 public class DesignController {
 
     @PostMapping
-    public void saveDesign(@RequestBody DesignDto designDto) {
-        System.out.println(designDto);
+    public void saveDesign(
+            @RequestParam(value = "title") String title,
+            @RequestParam(value = "file") MultipartFile file) {
+        System.out.println(title);
+        System.out.println(file);
     }
 
 }
