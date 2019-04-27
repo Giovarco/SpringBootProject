ccae0e7fe9fd5c6db69ba6cc7a074143f88c2fcc
Unknown <kirby94@hotmail.it>
2019-04-27 11:21:00 +0200

    Remove getters and setters

--- src/main/java/net/mignemi/portfolio/model/Book.java
+++ src/main/java/net/mignemi/portfolio/model/Book.java
@@ -13,37 +13,7 @@ public class Book {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String title;
-
-    public Book() {
-    }
-
-    public Book(String title) {
-        this.title = title;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    @Override
-    public String toString() {
-        return "Book{" +
-                "id=" + id +
-                ", name='" + title + '\'' +
-                ", price=" + price +
-                '}';
-    }
+    
 }
