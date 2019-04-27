1585ece581e70e9f6d8017d53e20d5daef454fb5
Unknown <kirby94@hotmail.it>
2019-04-27 15:37:49 +0200

    Save Design into the database with no image

--- src/main/java/net/mignemi/portfolio/controller/DesignController.java
+++ src/main/java/net/mignemi/portfolio/controller/DesignController.java
@@ -1,5 +1,8 @@
 package net.mignemi.portfolio.controller;
 
+import net.mignemi.portfolio.model.Design;
+import net.mignemi.portfolio.repository.DesignRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,12 +13,17 @@ import org.springframework.web.multipart.MultipartFile;
 @RequestMapping(value = "/design")
 public class DesignController {
 
+    @Autowired
+    DesignRepository designRepository;
+
     @PostMapping
     public void saveDesign(
             @RequestParam(value = "title") String title,
             @RequestParam(value = "file") MultipartFile file) {
-        System.out.println(title);
-        System.out.println(file);
+        Design design = Design.builder()
+                .title(title)
+                .build();
+        designRepository.save(design);
     }
 
 }
