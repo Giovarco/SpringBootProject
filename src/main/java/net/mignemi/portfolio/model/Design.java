efe0a28e844252201a3ebed2aea716cc6bd2f2ae
Unknown <kirby94@hotmail.it>
2019-04-27 15:35:26 +0200

    Add @Builder to Design entity

--- src/main/java/net/mignemi/portfolio/model/Design.java
+++ src/main/java/net/mignemi/portfolio/model/Design.java
@@ -1,5 +1,6 @@
 package net.mignemi.portfolio.model;
 
+import lombok.Builder;
 import lombok.Data;
 
 import javax.persistence.Column;
@@ -13,6 +14,7 @@ import javax.persistence.Table;
 @Entity
 @Table
 @Data
+@Builder
 public class Design {
 
     @Id
