173229532f9d5f1462fcd826d263293c43a0e738
Unknown <kirby94@hotmail.it>
2019-04-28 14:24:44 +0200

    Implement get tags end-point

--- src/main/java/net/mignemi/portfolio/controller/TagController.java
+++ src/main/java/net/mignemi/portfolio/controller/TagController.java
@@ -1,14 +1,18 @@
 package net.mignemi.portfolio.controller;
 
+import net.mignemi.portfolio.model.Design;
 import net.mignemi.portfolio.model.Tag;
 import net.mignemi.portfolio.service.TagService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/tag")
@@ -23,4 +27,8 @@ public class TagController {
         tagService.saveTag(tag);
     }
 
+    @GetMapping
+    public List<Tag> getDesigns() {
+        return tagService.getTags();
+    }
 }
