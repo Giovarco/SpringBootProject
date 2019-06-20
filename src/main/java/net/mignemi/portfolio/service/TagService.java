ef180176bf0c28b3c0af265c9b4827df92373b50
Unknown <kirby94@hotmail.it>
2019-06-20 14:57:48 +0200

    (WIP) Tag designs can be updated

--- src/main/java/net/mignemi/portfolio/service/TagService.java
+++ src/main/java/net/mignemi/portfolio/service/TagService.java
@@ -3,16 +3,21 @@ package net.mignemi.portfolio.service;
 import net.mignemi.portfolio.converter.TagDtoToTagConverter;
 import net.mignemi.portfolio.converter.TagToTagDtoConverter;
 import net.mignemi.portfolio.dto.TagDto;
+import net.mignemi.portfolio.model.Design;
 import net.mignemi.portfolio.model.Tag;
 import net.mignemi.portfolio.repository.DesignRepository;
 import net.mignemi.portfolio.repository.TagRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Iterator;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
+@Transactional
 public class TagService {
 
     @Autowired
@@ -47,7 +52,26 @@ public class TagService {
 
     public void updateTag(Long id, TagDto tagDto) {
         Tag tagToSave = tagRepository.getOne(id);
+        List<Design> oldDesigns = tagToSave.getDesigns();
+
         tagToSave.setTitle(tagDto.getTitle());
+
+        // Assign the new designs to the tag
+        List<Design> newDesigns = designRepository.findAllById(tagDto.getDesignIds());
+        tagToSave.setDesigns(newDesigns);
         tagRepository.save(tagToSave);
+
+        // Remove the tag from the old designs
+        List<Long> oldDesignIds = oldDesigns.stream().map(design -> design.getId()).collect(Collectors.toList());
+        List<Design> oldDesignEntities = designRepository.findAllById(oldDesignIds);
+        oldDesignEntities.stream().forEach(oldDesignEntity -> {
+            Iterator<Tag> i = oldDesignEntity.getTags().iterator();
+            while (i.hasNext()) {
+                Tag tag = i.next();
+                if(tag.getId()) // TODO
+                i.remove();
+            }
+        });
+        designRepository.saveAll(oldDesignEntities);
     }
 }
