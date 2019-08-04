ad12c5b388976ec31088d419d9a39041f5fced9c
Mario <kirby94@hotmail.it>
2019-08-04 15:02:37 +0200

    Fill database by CSV file

--- src/main/java/net/mignemi/portfolio/PortfolioApplication.java
+++ src/main/java/net/mignemi/portfolio/PortfolioApplication.java
@@ -1,14 +1,62 @@
 package net.mignemi.portfolio;
 
+import net.mignemi.portfolio.model.Film;
+import net.mignemi.portfolio.repository.FilmRepository;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 public class PortfolioApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PortfolioApplication.class, args);
-	}
+    @Autowired
+    FilmRepository filmRepository;
 
+    public static void main(String[] args) {
+        SpringApplication.run(PortfolioApplication.class, args);
+    }
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void fillDatabaseByCsvFile() throws IOException {
+        InputStream csvInputStream = getCsvFileAsInputStream();
+        CSVParser parser = getCSVParser(csvInputStream);
+
+        List<Film> filmList = new ArrayList<>();
+        for (CSVRecord record : parser) {
+            Film newFilm = Film.builder()
+                    .title(record.get(0))
+                    .locations(record.get(2)
+                    ).build();
+            filmList.add(newFilm);
+        }
+
+        filmRepository.saveAll(filmList);
+    }
+
+    private InputStream getCsvFileAsInputStream() throws IOException {
+        Resource resource = new ClassPathResource("Film_Locations_in_San_Francisco.csv");
+        return resource.getInputStream();
+    }
+
+    private CSVParser getCSVParser(InputStream input) throws IOException {
+        return CSVFormat.EXCEL
+                .withHeader()
+                .withDelimiter(';')
+                .withAllowMissingColumnNames(false)
+                .parse(new InputStreamReader(input));
+    }
 }
 
