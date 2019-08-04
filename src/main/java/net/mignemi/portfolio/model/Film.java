0a80c875fa158027475a388a47aa53f4c55e3dc7
Mario <kirby94@hotmail.it>
2019-08-04 13:52:11 +0200

    Update Film entity to fit requirements

--- src/main/java/net/mignemi/portfolio/model/Film.java
+++ src/main/java/net/mignemi/portfolio/model/Film.java
@@ -1,10 +1,6 @@
 package net.mignemi.portfolio.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table
@@ -13,47 +9,11 @@ public class Film  {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String name;
-    private Double price;
 
-    public Film() {
-    }
+    @Column
+    private String title;
 
-    public Film(String name, Double price) {
-        this.name = name;
-        this.price = price;
-    }
+    @Column
+    private String locations;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Double getPrice() {
-        return price;
-    }
-
-    public void setPrice(Double price) {
-        this.price = price;
-    }
-
-    @Override
-    public String toString() {
-        return "Film{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", price=" + price +
-                '}';
-    }
 }
