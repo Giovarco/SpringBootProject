2ce37aae23279465854895bf3aa3c1cf1beff5de
Mario <kirby94@hotmail.it>
2019-08-04 14:00:43 +0200

    Implement GET end-point

--- src/main/java/net/mignemi/portfolio/controller/Controller.java
+++ src/main/java/net/mignemi/portfolio/controller/Controller.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping(value = "/film")
 public class Controller {
@@ -17,6 +19,12 @@ public class Controller {
     @ResponseStatus(HttpStatus.CREATED)
     public void saveFilm(@RequestBody Film film) {
         filmRepository.save(film);
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<Film> getFilms() {
+        return filmRepository.findAll();
     }
 
 }
